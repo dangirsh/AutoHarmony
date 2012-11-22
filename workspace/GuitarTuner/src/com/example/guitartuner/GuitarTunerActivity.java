80da23d37f09cd125c67aa726f6aff5d142b30d8
Dan Girshovich <dan.girsh@gmail.com>
2012-11-22 15:42:16 -0500

    backup

--- workspace/GuitarTuner/src/com/example/guitartuner/GuitarTunerActivity.java
+++ workspace/GuitarTuner/src/com/example/guitartuner/GuitarTunerActivity.java
@@ -4,14 +4,18 @@ import java.io.File;
 import java.io.IOException;
 
 import org.puredata.android.io.AudioParameters;
-import org.puredata.android.io.PdAudio;
+import org.puredata.android.service.PdService;
 import org.puredata.android.utils.PdUiDispatcher;
 import org.puredata.core.PdBase;
 import org.puredata.core.PdListener;
 import org.puredata.core.utils.IoUtils;
 
 import android.os.Bundle;
+import android.os.IBinder;
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
@@ -24,18 +28,34 @@ public class GuitarTunerActivity extends Activity implements OnClickListener {
 	private PdUiDispatcher dispatcher;
 	Button aButton, bButton, dButton, eButton, gButton, eeButton;
 
+	private PdService pdService = null;
+
+	private final ServiceConnection pdConnection = new ServiceConnection() {
+
+		@Override
+		public void onServiceConnected(ComponentName name, IBinder service) {
+			pdService = ((PdService.PdBinder) service).getService();
+			try {
+				initPd();
+				loadPatch();
+			} catch (IOException e) {
+				Log.e(TAG, e.toString());
+				finish();
+			}
+		}
+
+		@Override
+		public void onServiceDisconnected(ComponentName name) {
+			// this method will never be called
+		}
+	};
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		initGui();
-
-		try {
-			initPd();
-			loadPatch();
-		} catch (IOException e) {
-			Log.e(TAG, e.toString());
-			finish();
-		}
+		bindService(new Intent(this, PdService.class), pdConnection,
+				BIND_AUTO_CREATE);
 	}
 
 	private void initGui() {
@@ -57,19 +77,17 @@ public class GuitarTunerActivity extends Activity implements OnClickListener {
 	private void initPd() throws IOException {
 		// Configure the audio glue
 		int sampleRate = AudioParameters.suggestSampleRate();
-		PdAudio.initAudio(sampleRate, 0, 2, 8, true);
-		PdAudio.initAudio(sampleRate, 1, 2, 8, true);
-		// Create and install the dispatcher 
+		// Create and install the dispatcher
+		pdService.initAudio(sampleRate, 1, 2, 10.0f);
 		dispatcher = new PdUiDispatcher();
 		PdBase.setReceiver(dispatcher);
 		dispatcher.addListener("pitch", new PdListener.Adapter() {
 			@Override
-			public void receiveFloat(String source, float x) { 
+			public void receiveFloat(String source, float x) {
 				Log.i(TAG, "pitch: " + x);
-			} 
-		}
-		);
-		
+			}
+		});
+
 	}
 
 	private void loadPatch() throws IOException {
@@ -80,18 +98,6 @@ public class GuitarTunerActivity extends Activity implements OnClickListener {
 		PdBase.openPatch(patchFile.getAbsolutePath());
 	}
 
-	@Override
-	protected void onResume() {
-		super.onResume();
-		PdAudio.startAudio(this);
-	}
-
-	@Override
-	protected void onPause() {
-		super.onPause();
-		PdAudio.stopAudio();
-	}
-
 	private void triggerNote(int n) {
 		PdBase.sendFloat("midinote", n);
 		PdBase.sendBang("trigger");
@@ -99,30 +105,32 @@ public class GuitarTunerActivity extends Activity implements OnClickListener {
 
 	@Override
 	public void onClick(View v) {
-		switch (v.getId()) { 
-			case R.id.e_button:
-				triggerNote(40); // E is MIDI note 40.
-				break;
-			case R.id.a_button:
-				triggerNote(45); // A is MIDI note 45.
-				break;
-			case R.id.d_button:
-				triggerNote(50); 
-				break;
-			case R.id.g_button:
-				triggerNote(55); 
-				break;
-			case R.id.b_button:
-				triggerNote(59); 
-				break;
-			case R.id.ee_button:
-				triggerNote(64);
-				break;
+		switch (v.getId()) {
+		case R.id.e_button:
+			triggerNote(40); // E is MIDI note 40.
+			break;
+		case R.id.a_button:
+			triggerNote(45); // A is MIDI note 45.
+			break;
+		case R.id.d_button:
+			triggerNote(50);
+			break;
+		case R.id.g_button:
+			triggerNote(55);
+			break;
+		case R.id.b_button:
+			triggerNote(59);
+			break;
+		case R.id.ee_button:
+			triggerNote(64);
+			break;
 		}
 	}
-	
+
 	@Override
-		public void onDestroy() {
-		super.onDestroy(); PdAudio.release(); PdBase.release();
+	public void onDestroy() {
+		super.onDestroy();
+		unbindService(pdConnection);
 	}
+
 }
