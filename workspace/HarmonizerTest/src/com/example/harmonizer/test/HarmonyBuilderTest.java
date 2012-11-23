ed3ce2cd9fbb40572f395b3957d16a479ad44340
Dan Girshovich <dan.girsh@gmail.com>
2012-11-23 16:01:46 -0500

    test commit

---
+++ workspace/HarmonizerTest/src/com/example/harmonizer/test/HarmonyBuilderTest.java
@@ -0,0 +1,23 @@
+package com.example.harmonizer.test;
+
+import junit.framework.TestCase;
+
+public class HarmonyBuilderTest extends TestCase {
+
+	public void testBuildHarmony() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetKey() {
+		fail("Not yet implemented");
+	}
+
+	public void testSetStyle() {
+		fail("Not yet implemented");
+	}
+
+	public void testSetKey() {
+		fail("Not yet implemented");
+	}
+
+}
